5018ad3a2024e49d7f8416ae59fafbc05179704c
Emilien-Marot <12394@holbertonstudents.com>
2026-02-26 11:38:59 +0400

    type and verification for user and amenities

--- part2/hbnb/app/models/amenities.py
+++ part2/hbnb/app/models/amenities.py
@@ -3,6 +3,10 @@ from baseModel import BaseModel
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
+        if not isinstance(name, str):
+            raise TypeError("name must be a string")
+        if len(name) > 50:
+            raise ValueError("name must not be more than 50 characters")
         self.name = name
 
     def save(self):
--- part2/hbnb/app/models/users.py
+++ part2/hbnb/app/models/users.py
@@ -3,9 +3,22 @@ from baseModel import BaseModel
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin = False):
         super().__init__()
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        if len(first_name) > 50:
+            raise ValueError("first_name must not be more than 50 characters")
         self.first_name = first_name
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+        if len(last_name) > 50:
+            raise ValueError("last_name must not be more than 50 characters")
         self.last_name = last_name
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+        #TODO verify email
         self.email = email
+        if not isinstance(is_admin, bool):
+            raise TypeError("is_admin must be a boolean")
         self.is_admin = is_admin
         self.places = []
 
