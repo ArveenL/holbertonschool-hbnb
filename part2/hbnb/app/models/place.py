1fdf8b44d033bf397f2897a16e2e426499972121
Emilien-Marot <12394@holbertonstudents.com>
2026-02-26 12:29:51 +0400

    correction of place.py and test

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,10 +1,8 @@
 from app.models.baseModel import BaseModel
-from app.models.user import User
-from app.models.review import Review
-from app.models.amenity import Amenity
+
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, title, description, price, latitude, longitude, owner: 'User'):
         super().__init__()
         if not isinstance(title, str):
             raise TypeError("title must be a string")
@@ -14,21 +12,19 @@ class Place(BaseModel):
         if not isinstance(description, str):
             raise TypeError("description must be a string")
         self.description = description
-        if not isinstance(price, float):
+        if not isinstance(price, (float, int)):
             raise TypeError("price must be a number")
         self.price = price
-        if not isinstance(latitude, str):
+        if not isinstance(latitude, (float, int)):
             raise TypeError("latitude must be a number")
         if latitude > 90 or latitude < -90:
             raise ValueError("latitude must be between -90 and 90")
         self.latitude = latitude
-        if not isinstance(longitude, str):
+        if not isinstance(longitude, (float, int)):
             raise TypeError("longitude must be a number")
         if longitude > 180 or longitude < -180:
             raise ValueError("longitude must be between -180 and 180")
         self.longitude = longitude
-        if not isinstance(title, User):
-            raise TypeError("owner must be a User")
         self.owner = owner
         self.reviews = []
         self.amenities = []
@@ -39,14 +35,10 @@ class Place(BaseModel):
     def update(self, data):
         super().update(data)
 
-    def add_review(self, review):
+    def add_review(self, review: 'Review'):
         """Add a review to the place."""
-        if not isinstance(review, Review):
-            raise TypeError("review must be a Review")
         self.reviews.append(review)
 
-    def add_amenity(self, amenity):
+    def add_amenity(self, amenity: 'Amenity'):
         """Add an amenity to the place."""
-        if not isinstance(amenity, Amenity):
-            raise TypeError("amenity must be an Amenity")
         self.amenities.append(amenity)
--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,9 +1,8 @@
 from app.models.baseModel import BaseModel
-from app.models.place import Place
-from app.models.user import User
+
 
 class Review(BaseModel):
-    def __init__(self, text, rating, place, user):
+    def __init__(self, text, rating, place: 'Place', user: 'User'):
         super().__init__()
         if not isinstance(text, str):
             raise TypeError("text must be a string")
@@ -13,11 +12,7 @@ class Review(BaseModel):
         if rating not in range(1, 6):
             raise ValueError("rating must be between 1 and 5")
         self.rating = rating
-        if not isinstance(place, Place):
-            raise TypeError("title must be a string")
         self.place = place
-        if not isinstance(user, User):
-            raise TypeError("amenity must be an Amenity")
         self.user = user
 
     def save(self):
