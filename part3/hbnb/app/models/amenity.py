8948846774e45303933ff17bc85553cbe1ab3b4d
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-02 10:24:33 +0400

    remove exess table from place_amenity

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,12 +1,6 @@
 from app import db
 from app.models.baseModel import BaseModel
 
-# Association table for Place <-> Amenity
-place_amenity = db.Table(
-    'place_amenity',
-    db.Column('place_id', db.String(36), db.ForeignKey('places.id'), primary_key=True),
-    db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
-)
 
 class Amenity(BaseModel):
     __tablename__ = "amenities"
@@ -16,7 +10,7 @@ class Amenity(BaseModel):
     # Relationship with Place
     places = db.relationship(
         'Place',
-        secondary=place_amenity,
+        secondary='place_amenity',
         lazy='subquery',
         backref=db.backref('amenities', lazy=True)
     )
