c322f318c9c722e22e5ed8654eda32da451fc10d
Emilien-Marot <12394@holbertonstudents.com>
2026-04-06 09:46:40 +0400

    task 1 (untested) and corrected HTML

---
+++ part4/scripts.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      let email = document.getElementById("email").value;
+      let password = document.getElementById("password").value;
+      console.log(email)
+      console.log(password)
+      loginUser(email, password);
+    });
+  }
+});
+
+async function loginUser(email, password) {
+  const response = await fetch('http://127.0.0.1:5678/api/v1/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password })
+  });
+  if (response.ok) {
+    console.log(response)
+    const data = await response.json();
+    document.cookie = `token=${data.access_token}; path=/`;
+    window.location.href = 'index.html';
+  } else {
+    alert('Login failed: ' + response.statusText);
+  }
+}
