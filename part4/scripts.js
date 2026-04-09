05ff50a0a3971e3c7e66e0bf4a992ceaf07d0235
Emilien-Marot <12394@holbertonstudents.com>
2026-04-09 10:50:17 +0400

    restore old script

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,3 +1,18 @@
+async function loginUser(email, password) {
+  const response = await fetch('http://127.0.0.1:5678/api/v1/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password })
+  });
+  if (response.ok) {
+    const data = await response.json();
+    window.localStorage.setItem("token", data.access_token)
+    window.location.href = 'index.html';
+  } else {
+    alert('Login failed: ' + response.statusText);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('login-form');
 
@@ -6,26 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
       event.preventDefault();
       let email = document.getElementById("email").value;
       let password = document.getElementById("password").value;
-      console.log(email)
-      console.log(password)
       loginUser(email, password);
     });
   }
 });
-
-async function loginUser(email, password) {
-  const response = await fetch('http://127.0.0.1:5678/api/v1/auth/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
-  });
-  if (response.ok) {
-    console.log(response)
-    const data = await response.json();
-    document.cookie = `token=${data.access_token}; path=/`;
-    window.location.href = 'index.html';
-  } else {
-    alert('Login failed: ' + response.statusText);
-  }
-}
-
